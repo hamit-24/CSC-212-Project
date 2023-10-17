5f16e9dd69a7b21badd37f5ef9aadbd0e07c4163
Admin <Admin@DESKTOP-IH775JU>
2023-10-17 22:44:36 +0300

    added our names and project name to class Node (:

--- csc212/src/Node.java
+++ csc212/src/Node.java
@@ -1,4 +1,24 @@
+/*
+CLASS: PhaseOne.java
 
+CSC212 Data structures - Project phase I
+
+Fall 2023
+
+EDIT DATE:
+
+17-10-2023
+
+TEAM:
+
+.next
+
+AUTHORS:
+
+Hamad allaeboon , (ID443100096)
+Abdullah alabdulkareem, (ID443102379)
+Mshari almuammar, (ID443101420)
+ */
 public class Node<T> {
 	public T data;
 	public Node<T> next;
